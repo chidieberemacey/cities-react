41a98ef6cc748f89e8892a73d24a1ee382e9bddd
Chidiebere Macey <egbomaceyskyla@gmail.com>
2024-01-27 08:44:54 +0100

    27th jan

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -31,11 +31,6 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             {item}
           </li>
         ))}
-        {/* <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li> */}
       </ul>
     </>
   );
